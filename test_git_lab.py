d5d9c6b7fe4ec81a48852171729935d5b05a241b
23clhelle <chellebusch2@unl.edu>
2025-02-18 15:00:43 -0600

    created multiply_number function and added a test for it

--- test_git_lab.py
+++ test_git_lab.py
@@ -5,7 +5,8 @@ from git_lab import (
     is_palindrome,
     calculate_triangle_area,
     is_prime,
-    add_numbers
+    add_numbers,
+    multiply_numbers
 )
 import math
 
@@ -52,6 +53,10 @@ class TestUtils(unittest.TestCase):
     def test_add_numbers(self):
         self.assertEqual(add_numbers(2, 3), 5)
         self.assertEqual(add_numbers(-1, 1), 0)
+
+    def test_multiply_numbers(self):
+        self.assertEqual(multiply_numbers(2, 3), 6)
+        self.assertEqual(multiply_numbers(-1, 1), -1)
 
 if __name__ == '__main__':
     unittest.main()
